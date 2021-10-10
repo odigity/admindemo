e06a911967e24ef811685c7cd7e6e42e0e01e3cf
Ofer Nave <ofer.nave@goalbasedinvestors.com>
2021-10-10 18:37:25 -0500

    registered models with Django Admin

--- worlds/admin.py
+++ worlds/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Character, Episode, Medium, Series, UnitOfFiction, Universe
+
 # Register your models here.
+admin.site.register(Character)
+admin.site.register(Episode)
+admin.site.register(Medium)
+admin.site.register(Series)
+admin.site.register(UnitOfFiction)
+admin.site.register(Universe)
