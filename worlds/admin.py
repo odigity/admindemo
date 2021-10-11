b4927865f9549ef4196af1f68a9f7f848716f23b
Ofer Nave <ofer.nave@goalbasedinvestors.com>
2021-10-11 09:41:31 -0500

    added empty <Model>Admin classes to facilitate customization

--- worlds/admin.py
+++ worlds/admin.py
@@ -2,10 +2,34 @@ from django.contrib import admin
 
 from .models import Character, Episode, Medium, Series, UnitOfFiction, Universe
 
-# Register your models here.
-admin.site.register(Character)
-admin.site.register(Episode)
-admin.site.register(Medium)
-admin.site.register(Series)
-admin.site.register(UnitOfFiction)
-admin.site.register(Universe)
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Episode)
+class EpisodeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Medium)
+class MediumAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Series)
+class SeriesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UnitOfFiction)
+class UnitOfFictionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Universe)
+class UniverseAdmin(admin.ModelAdmin):
+    pass
+
+
