dd8cb62d71a25eac585d2165d4b4d5c09ffffc80
Ofer Nave <ofer.nave@goalbasedinvestors.com>
2021-10-11 12:37:36 -0500

    added in-universe friends filtering, and some sample data from a second universe to demonstrate

--- worlds/admin.py
+++ worlds/admin.py
@@ -1,11 +1,17 @@
+from django import forms
 from django.contrib import admin
 
 from .models import Character, Episode, Medium, Series, UnitOfFiction, Universe
 
 
+class CharacterAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['friends'].queryset = Character.objects.filter(universe=self.instance.universe)
+
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
-    pass
+    form = CharacterAdminForm
 
 
 @admin.register(Episode)
@@ -31,5 +37,3 @@ class UnitOfFictionAdmin(admin.ModelAdmin):
 @admin.register(Universe)
 class UniverseAdmin(admin.ModelAdmin):
     pass
-
-
